430f23fc05d466f30da220215e51e3bc5bded3f1
ChrisBerry1234 <christopherjberry846@gmail.com>
2025-12-02 02:45:38 -0500

    In add transactions functions in app.py-created if else statement to render template for addtransactions.html and to append user input from form in our list data structure. Works successfully

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 TRANSACTIONS = [
     {'id': 1, 'date': '2023-06-01', 'amount': 100},
-    {'id': 1, 'date': '2023-06-02', 'amount': 200},
-    {'id': 1, 'date': '2023-06-03', 'amount': 300}
+    {'id': 2, 'date': '2023-06-02', 'amount': 200},
+    {'id': 3, 'date': '2023-06-03', 'amount': 300}
 ]
 
 
@@ -13,7 +13,20 @@ app = Flask(__name__)
 def index():
     return render_template("transactions.html", data = TRANSACTIONS)
 
-@app.route("/add_transaction")
-def add():
-    return render_template("addtransaction.html")
+@app.route("/add_transaction", methods=["GET","POST"])
+def add_transactions():
+
+    if request.method == "GET":
+        return render_template("addtransaction.html")
+
+    transaction = {
+        'id': len(TRANSACTIONS)+1,
+        'date': request.form.get("date"),
+        'amount': request.form.get("amount"),
+    }
+
+    TRANSACTIONS.append(transaction)
+
+    return redirect(url_for("index"))
+
 app.run(debug=True)
